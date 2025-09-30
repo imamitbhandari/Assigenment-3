2798646416c4fa9381fc5cc87d7b726338d14806
Amit <93990000+imamitbhandari@users.noreply.github.com>
2025-09-30 23:54:10 +1300

    Amit part B resit.py

--- Amit part B resit.py
+++ Amit part B resit.py
@@ -1,10 +1,12 @@
 #Booking system
 class BookingSystem:
     #setting counter
+    # Used DRY for a global counter of unique ticket ids
     counter = 1 
 
     def __init__(self):
         # booking for instance attrributes for each
+        # used Encapsulation this method only store booking 
         self.form_of_id = ""
         self.id_number = ""
         self.passenger_name = ""
@@ -14,6 +16,7 @@ class BookingSystem:
         self.approval_reference = ""
 
 #customer details
+# used SRP because this method is responsible for collecting customer information
     def customer_info(self):
         BookingSystem.counter += 1
         self.form_of_id = input("Form of ID (Passport, driver's license): ")
@@ -22,6 +25,7 @@ class BookingSystem:
         self.ticket_id = BookingSystem.counter + 20000
 
 #Gather ferry service details and calculate total cost
+#used SRP because this method only calculates the total service cost
     def ferry_service_details(self):
         self.total = 0
         number_items = int(input("Enter number of services: "))
@@ -32,6 +36,7 @@ class BookingSystem:
             self.total += price
 
 #A booking request based on user input for approves or rejects
+#used SRP because this method only manages approval or rejection process
     def respond_requsition(self):
         # It process only  if the booking is still pending
         if self.status == "Pending": 
@@ -45,6 +50,7 @@ class BookingSystem:
                 self.approval_reference = "Not available"
 
     # This line is  for display the full booking information
+    #  used SRP because this method is only used to display booking information
     def display_booking_info(self):
         print(f"Form of ID (Passport, driver's license): {self.form_of_id}")
         print(f"ID Number: {self.id_number}")
@@ -55,6 +61,7 @@ class BookingSystem:
         print(f"Approval Reference Number: {self.approval_reference}")
 
 #calling all the functions
+#used KISS because main program flow is kept simple and easy to follow
 booking = BookingSystem()
 booking.customer_info()
 booking.ferry_service_details()
